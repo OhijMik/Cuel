28d909d0cc02e3f22b4ba339126c078be72750e2
kimjih36 <jihok148@gmail.com>
2024-12-18 14:45:26 -0500

    Made the cube be adjustable
    - Adjust the size of the cube
    - Adjust the range of the cube

--- Cuel/Assets/Scripts/CubeSpawner.cs
+++ Cuel/Assets/Scripts/CubeSpawner.cs
@@ -11,11 +11,15 @@ public class CubeSpawner : MonoBehaviour
 
     [SerializeField] private int indexToSpawn = 0;
     [SerializeField] private LayerMask despawnLayer;
+    private GameObject tempCube;
+    private float cubeScale = 0.5f;
+    private float range = 5f;
 
     private void Awake()
     {
         avatar = GetComponent<Alteruna.Avatar>();
         spawner = GameObject.FindGameObjectWithTag("NetworkManager").GetComponent<Spawner>();
+        tempCube = GameObject.FindGameObjectWithTag("TempCube");
     }
 
     private void Update()
@@ -25,21 +29,56 @@ public class CubeSpawner : MonoBehaviour
             return;
         }
 
-        if (Input.GetKeyDown(KeyCode.F))
+        // Scale the cube size
+        if (Input.GetKey(KeyCode.F))
+        {
+            if (cubeScale < 10f)
+            {
+                cubeScale += 0.05f;
+            }
+        }
+
+        // Spawn Cube
+        if (Input.GetKeyUp(KeyCode.F))
         {
             SpawnCube();
+        }
+
+        // Increase the range
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (range < 30f)
+            {
+                range += 0.4f;
+            }
+        }
+
+        // Decrease the range
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            if (range > 5f)
+            {
+                range -= 0.4f;
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.Q))
         {
             DespawnCube();
         }
+
+        Debug.Log(Camera.main.transform.position);
+
+        tempCube.transform.localScale = new Vector3(cubeScale, cubeScale, cubeScale);
+        tempCube.transform.position = Camera.main.transform.position + Camera.main.transform.forward * range;
+        tempCube.transform.rotation = Camera.main.transform.rotation;
     }
 
     private void SpawnCube()
     {
-        spawner.Spawn(indexToSpawn, Camera.main.transform.position + Camera.main.transform.forward * 1.5f,
-                      Camera.main.transform.rotation, new Vector3(0.5f, 0.5f, 0.5f));
+        spawner.Spawn(indexToSpawn, Camera.main.transform.position + Camera.main.transform.forward * range,
+                      Camera.main.transform.rotation, new Vector3(cubeScale, cubeScale, cubeScale));
+        cubeScale = 0.5f;
     }
 
     private void DespawnCube()
