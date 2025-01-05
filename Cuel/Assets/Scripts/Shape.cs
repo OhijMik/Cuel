d04b688ce78da7639baae18ee6f6bd116ae9b64c
kimjih36 <jihok148@gmail.com>
2025-01-04 21:45:01 -0500

    Fixed a bug with null reference to player

--- Cuel/Assets/Scripts/Shape.cs
+++ Cuel/Assets/Scripts/Shape.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 
 public class Shape : MonoBehaviour
 {
-    [SerializeField] private float damage = 10f;
+    private float damage;
+    private float health;
     private bool hit = false;
     private Player owner;
 
+    public void SetOwner(Player owner)
+    {
+        this.owner = owner;
+
+    }
+
     private void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.tag == "owner" && !hit && col.gameObject.GetComponent<Shape>().GetOwner() != owner)
+        if (col.gameObject.tag == "Player" && !hit && col.gameObject.GetComponent<Player>() != owner)
         {
-            Player owner = col.gameObject.GetComponent<Player>();
-            owner.Hit(damage);
+            Player player = col.gameObject.GetComponent<Player>();
+            player.Hit(damage);
             hit = true;
         }
         else if (tag == "Ball" && col.gameObject.tag == "Cube" && col.gameObject.GetComponent<Shape>().GetOwner() != owner)
         {
             Destroy(col.gameObject);
         }
-        else if (tag == "Triangle" && col.gameObject.tag == "Cube")
+        else if (tag == "Triangle" && col.gameObject.tag == "Ball" && col.gameObject.GetComponent<Shape>().GetOwner() != owner)
         {
             Destroy(col.gameObject);
         }
@@ -29,11 +36,6 @@ public class Shape : MonoBehaviour
         {
             hit = true;
         }
-    }
-
-    public void SetOwner(Player owner)
-    {
-        this.owner = owner;
     }
 
     public Player GetOwner()
